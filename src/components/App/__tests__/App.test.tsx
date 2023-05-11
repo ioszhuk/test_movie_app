85d778ad8f61e4446d053d6623b9725063745f72
ioszhuk <ioszhuk@gmail.com>
2023-05-11 04:51:45 +0200

    refactor

--- src/components/App/__tests__/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from '../App';
-
-it('renders without crashing', () => {
-  const root = ReactDOM.createRoot(document.createElement('div') as HTMLElement);
-  root.render(<App />);
-});
